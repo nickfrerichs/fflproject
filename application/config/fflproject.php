4a1e09e5e5b6037af90e661e2a94f8ebbde7843d
Nick Frerichs <frerichs@gmail.com>
2016-06-19 18:52:13 -0500

    Forgot to include fflproject.php config file.

---
+++ application/config/fflproject.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Need some variables from consolidated config file.
+include(FCPATH.'config.php');
+
+$config['basic_debug'] = $fflp_debug;
