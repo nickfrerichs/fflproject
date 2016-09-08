e854de3d14a0bc930009342132c6afd35b7ecec0
Nick Frerichs <frerichs@gmail.com>
2016-09-07 21:43:51 -0500

    Standings now show up even if no games have completed.

--- application/controllers/season/Standings.php
+++ application/controllers/season/Standings.php
@@ -39,13 +39,23 @@ class Standings extends MY_User_Controller{
                         <tr><td colspan=5 style="background-color:#DDDDDD;"><strong><?=$d['name']?></strong></td></tr>
                     <?php endif;?>
                     <?php foreach ($d['standings'] as $t): ?>
-                        <?php $winptc = ($t->wins+($t->ties/2)) / $t->total_games;?>
+                        <?php if ($t->total_games == 0)
+                            {
+                                $winptc=.000;
+                                $avgpts=0;
+                                $avgopp=0;
+                            }
+                            else{
+                                $winptc = ($t->wins+($t->ties/2)) / $t->total_games;
+                                $avgpts = round($t->points/$t->total_games,1);
+                                $avgopp = round($t->opp_points/$t->total_games,1);
+                            }?>
                         <tr>
                             <td><?=isset($t->notation_symbol) ? $t->notation_symbol.' ' : ''?><?=$t->team_name?></td>
                             <td><?=str_replace('0.','.',number_format($winptc,3))?></td>
                             <td><?=$t->wins?>-<?=$t->losses?>-<?=$t->ties?></td>
-                            <td><?=$t->points?> <span style="font-size:.8em">(<?=round($t->points/$t->total_games,1)?>)</span></td>
-                            <td><?=$t->opp_points?> <span style="font-size:.8em">(<?=round($t->opp_points/$t->total_games,1)?>)</span></td>
+                            <td><?=$t->points?> <span style="font-size:.8em">(<?=$avgpts?>)</span></td>
+                            <td><?=$t->opp_points?> <span style="font-size:.8em">(<?=$avgopp?>)</span></td>
                         </tr>
                     <?php endforeach;?>
                 </tbody>
