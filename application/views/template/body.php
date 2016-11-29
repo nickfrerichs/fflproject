09ef8e71fdc5128128a86b9147aad9d8d95204a0
Nick Frerichs <frerichs@gmail.com>
2016-11-29 15:35:56 -0600

    Added a max delay in waiting for intial ajax loads to finish.  Should prevent a blank screen if something goes wrong with a background ajax request.

--- application/views/template/body.php
+++ application/views/template/body.php
@@ -20,6 +20,9 @@
 <script>
 function js_Load(){
     // Wait for all ajax_waits to be false so the page is only displayed when everything is loaded
+    var max_check_time = 1000;
+    var total_check_time = 0;
+    var check_interval = 25;
     var check = function(){
         debug_out(ajax_waits);
         // Check each one, if one found, ajax_wait is true.
@@ -29,13 +32,14 @@ function js_Load(){
             if(ajax_waits[key] == true){ajax_wait=true; break;}
         }
         // Unless ajax_wait is false, repeat.  If false, we're all loaded, show the page.
-        if(ajax_wait == false){
+        if(ajax_wait == false || total_check_time >= max_check_time){
             document.body.style.visibility='visible';
             return;
             // run when condition is met
         }
         else {
-            setTimeout(check, 25); // check again in a second
+            total_check_time+=check_interval
+            setTimeout(check, check_interval); // check again in a second
         }
     }
     check();
