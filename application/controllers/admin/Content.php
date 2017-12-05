b5546ff9f8e64d63a37c45a7b1f527be18c3b1e9
Nick Frerichs <frerichs@gmail.com>
2017-12-05 09:01:45 -0600

    Admin post season content editor needed updating from the past offseason

--- application/controllers/admin/Content.php
+++ application/controllers/admin/Content.php
@@ -31,6 +31,8 @@ class Content extends MY_Admin_Controller
     {
         if ($text_id == "news")
             redirect('admin/content/news');
+        if ($text_id == "playoffs")
+            redirect('admin/content/postseason');
         $data = array();
         $data['content'] = $this->content_model->get_page_data($text_id);
         $data['text_id'] = $text_id;
@@ -43,15 +45,14 @@ class Content extends MY_Admin_Controller
     function postseason($year = 0)
     {
         $data = array();
-        $data['selected_year'] = $year;
+
 
         if ($year == 0)
             $year = $this->current_year;
         
-
+        $data['selected_year'] = $year;
         $data['content'] = $this->content_model->get_postseason_data($year);
         $data['years'] = $this->common_model->get_league_years();
-
 
         $this->bc['Content'] = site_url('admin/content');
         $this->bc['Post Season'] = site_url('admin/content/postseason');
@@ -67,7 +68,23 @@ class Content extends MY_Admin_Controller
         if($year == 0)
             $year = $this->current_year;
 
-        
+        $this->bc['Content'] = site_url('admin/content');
+        $this->bc['Post Season '.$year] = "";
+
+        $data['content'] = $this->content_model->get_postseason_data($year);
+        if (empty($data['content']))
+            redirect(site_url('admin/content/postseason'));
+
+        $this->admin_view('admin/content/edit',$data);
+
+    }
+
+    function create_postseason($year = 0)
+    {
+        if ($year == 0)
+            $year = $this->current_year;
+        $this->content_model->create_postseason_content($year);
+        redirect(site_url('admin/content/edit_postseason/'.$year));
     }
 
     function news()
