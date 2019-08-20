9e4f9fe25cca195beb1f7f0563a26a7f7e1d3e32
Nick Frerichs <frerichs@gmail.com>
2019-08-20 16:37:35 -0500

    Semicolon

--- application/config/fflproject.php
+++ application/config/fflproject.php
@@ -7,6 +7,5 @@ $config['basic_debug'] = $fflp_debug;
 $config['use_recaptcha'] = $fflp_use_recaptcha;
 $config['fflp_email_reply_to'] = $fflp_email_reply_to;
 $config['fflp_email_site_title'] = $fflp_email_site_title;
-
-$config['fflp_recaptcha_public_key'] = $fflp_recaptcha_public_key
-$config['fflp_recaptcha_private_key'] = $fflp_recaptcha_private_key
+$config['fflp_recaptcha_public_key'] = $fflp_recaptcha_public_key;
+$config['fflp_recaptcha_private_key'] = $fflp_recaptcha_private_key;
