5a0a5c042f60856ab95ade510b1a6b677dc7b2c5
Nick Frerichs <frerichs@gmail.com>
2019-08-20 16:34:03 -0500

    Removed test captcha keys

--- application/config/fflproject.php
+++ application/config/fflproject.php
@@ -8,5 +8,5 @@ $config['use_recaptcha'] = $fflp_use_recaptcha;
 $config['fflp_email_reply_to'] = $fflp_email_reply_to;
 $config['fflp_email_site_title'] = $fflp_email_site_title;
 
-$config['fflp_recaptcha_public_key'] = '6Ld0AvkSAAAAAAGdIMNJdfY-9enMGF5VYjkfdzFI';
-$config['fflp_recaptcha_private_key'] = '6Ld0AvkSAAAAAOnAGxmAJulitvlDAnLQ6rOKkmHy';
+$config['fflp_recaptcha_public_key'] = $fflp_recaptcha_public_key
+$config['fflp_recaptcha_private_key'] = $fflp_recaptcha_private_key
