e7a32dcddebad50dccc00a0ae2f38bc67e2a2be8
Nick Frerichs <frerichs@gmail.com>
2020-09-08 10:42:52 -0500

    Preparing for rocky 2020 season

---
+++ python/queries.py
@@ -0,0 +1,61 @@
+def GET_PLAYERS(first, season, after=None):
+    q = """
+    {
+        viewer{
+            players(first:%s *AFTER* season_season:%s){
+                edges{
+                    node{
+                        id
+                        person{
+                            firstName
+                            lastName
+                            middleName
+                            nickName
+                            birthDate
+                            gsisId
+                            esbId
+                            collegeName
+                            headshot{
+                                url
+                            }
+                            currentPlayer{
+                                id
+                                gsisId
+                                currentTeam{
+                                    id
+                                    abbreviation
+                                }
+                                height
+                                weight
+                                jerseyNumber
+                                nflExperience
+                                position
+                                status
+                            }
+                            property{
+                                enabled
+                            }
+                            status
+                        } 
+                    }
+                }
+                pageInfo {
+                    hasNextPage
+                    hasPreviousPage
+                    startCursor
+                    endCursor
+                    previousPageStartCursor
+                    total
+                }
+            }
+        }
+    }
+
+    """ % (str(first), str(season))
+
+    if after is not None:
+        q = q.replace("*AFTER*",'after:"'+after+'"')
+    else:
+        q = q.replace("*AFTER*","")
+
+    return q
